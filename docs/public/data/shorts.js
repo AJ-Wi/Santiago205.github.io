76ffaae0f37a327194a266548415503cc523faf6
wipodev <ajwipo@gmail.com>
2023-07-26 00:24:54 -0500

    creacion de data de ejemplo

---
+++ docs/public/data/shorts.js
@@ -0,0 +1,18 @@
+export const Shorts = [
+  {
+    title: "Minecraft pero NO puedo tocar el color AMARILLO",
+    link: "d_ocOE-OFQ4",
+  },
+  {
+    title: "SEGUNDAS MEGA APERTURA DE RULETAS EN Stumble Guys",
+    link: "Wx7nDfxPotI",
+  },
+  {
+    title: "ABRIENDO EL PASE DE Stumble Guys parte 2",
+    link: "y3bkPIvvNXE",
+  },
+  {
+    title: "ABRIENDO EL PASE DE Stumble Guys parte 1",
+    link: "grYrpZa1XIM",
+  },
+];
